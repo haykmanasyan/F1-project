c7463b9004af414f12e4569aca4703847ec00035
cloudstatic01 <cloudstatic01@gmail.com>
2023-10-24 16:05:57 -0400

    second commit

--- Driver.java
+++ Driver.java
@@ -5,37 +5,52 @@ public class Driver{
     private int podiums;
     private int points;
 
-    public void win(){
+    private Team team;
+
+    public Driver(String acronym, Team team) {
+
+        this.acronym = acronym;
+        this.team = team;
+    }
+
+    public void win() {
+
         wins++;
     }
 
     public void achievePodium() {
+
         podiums++;
     }
 
     public void earnPoints(int pointsEarned) {
+
         points += pointsEarned;
     }
 
     public String getName() {
+
         return acronym;
     }
 
     public int getWins() {
+
         return wins;
     }
 
     public int getPodiums() {
+
         return podiums;
     }
 
     public int getPoints() {
+
         return points;
     }
 
-    public void setName(String name) {
-        this.acronym = name;
+    public Team getTeam() {
+        
+        return team;
     }
-
-    
+ 
 }
---
+++ Team.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    
+    private String name;
+    private List<Driver> drivers;
+    private int totalPoints;
+
+    public Team(String name) {
+
+        this.name = name;
+        this.drivers = new ArrayList<>();
+        this.totalPoints = 0;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void addDriver(Driver driver) {
+
+        drivers.add(driver);
+    }
+
+    public List<Driver> getDrivers() {
+
+        return drivers;
+    }
+
+    public void calculateTotalPoints() {
+
+        totalPoints = 0;
+
+        for (Driver driver : drivers) {
+            totalPoints += driver.getPoints();
+        }
+    }
+
+    public int getTotalPoints() {
+        
+        return totalPoints;
+    }
+}
